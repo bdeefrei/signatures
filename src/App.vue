746241e7353035d901ec9f09d4ed153575cc240e
Alexandre Martin <alexandre@m4rtin.xyz>
2017-04-27 16:37:57 +0200

    Add the form to the app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <div class="container">
+      <generator></generator>
+    </div>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/Header'
+import Generator from './components/Generator'
 
 export default {
   name: 'app',
   components: {
-    AppHeader
+    AppHeader,
+    Generator
   }
 }
 </script>
---
+++ src/components/Generator.vue
@@ -0,0 +1,52 @@
+<template>
+  <form class="form-horizontal">
+    <fieldset>
+      <legend>Générez votre signature</legend>
+      <div class="form-group">
+        <label for="inputFirstName" class="col-lg-2 control-label">Prénom</label>
+        <div class="col-lg-10">
+          <input class="form-control" id="inputFirstName" placeholder="John" type="text">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="inputLastName" class="col-lg-2 control-label">Nom</label>
+        <div class="col-lg-10">
+          <input class="form-control" id="inputLastName" placeholder="Doe" type="text">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+        <div class="col-lg-10">
+          <input class="form-control" id="inputEmail" placeholder="john.doe@team-scylla.fr" type="text">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="inputPhone" class="col-lg-2 control-label">Téléphone</label>
+        <div class="col-lg-10">
+          <input class="form-control" id="inputPhone" placeholder="06 00 00 00 00" type="text">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="inputRole" class="col-lg-2 control-label">Poste</label>
+        <div class="col-lg-10">
+          <input class="form-control" id="inputRole" placeholder="Président" type="text">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="inputlinkedIn" class="col-lg-2 control-label">Lien de votre LinkedIn</label>
+        <div class="col-lg-10">
+          <input class="form-control" id="inputLinkedIn" placeholder="https://linkedin.com/..." type="text">
+        </div>
+      </div>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+
+</script>
