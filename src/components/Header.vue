aae0762b57bb8805b1c0fabc6dd32f14effaac1c
Alexandre Martin <alexandre@m4rtin.xyz>
2017-04-27 16:25:15 +0200

    Make the header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,14 @@
 <template>
   <nav class="navbar navbar-default">
     <div class="container-fluid">
+
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Ouvrir la navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
         <a class="navbar-brand" href="/">Générateur de signature</a>
       </div>
 
