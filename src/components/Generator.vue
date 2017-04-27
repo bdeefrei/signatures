c17695a49c4708fcfa64c3d8f80fbe8d249e87a5
Alexandre Martin <alexandre@m4rtin.xyz>
2017-04-28 00:01:49 +0200

    Finish the alpha version

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -49,28 +49,26 @@
 
     <div class="panel panel-default first">
       <div class="panel-heading">Voici votre signature</div>
-      <div class="panel-body">
-        <div style="float: left; margin: 2px 5px 5px 0px; padding-right: 5px; display: block;" id="photoWrapper">
-          <img src="https://public.alexandre-martin.fr/BDE/logo.png" width="65px" height="65px" id="sigPhoto">
+      <div class="panel-body" id="signature">
+        <div style="float: left; margin: 2px 5px 5px 0px; padding-right: 5px; display: block;">
+          <img src="https://public.alexandre-martin.fr/BDE/logo.png" width="90px" height="90px">
         </div>
-        <div style="margin-top:0px; margin-left: 74px;" id="sigDetailsWrapper">
+        <div style="margin-top:0px; margin-left: 110px">
           <p style="font-family: Helvetica, sans-serif; font-size: 12px; line-height: 14px; color: #333; margin-top:0; margin-left:0; padding-left:0;">
             <strong>
-              <span id="sigName" v-if="displayName">{{ inputFirstName }} {{ inputLastName }}</span>
-              <span id="sigName" v-else>John Doe</span>
+              <span v-if="displayName">{{ inputFirstName }} {{ inputLastName }}</span>
+              <span v-else>John Doe</span>
             </strong>
             <span v-if="displayRole">/ {{ inputRole }} BDE Scylla</span>
             <span v-else>/ Président BDE Scylla</span><br>
-            <span v-if="displayMail"> <a v-bind:href="'mailto:' +  inputEmail" id="sigEmail" style="color:#428BCA;">{{ inputEmail }}</a></span>
-            <span v-else><a href="mailto:john.doe@team-scylla.fr" id="sigEmail" style="color:#428BCA;">john.doe@team-scylla.fr</a></span>
-            <span id="sigMobile"></span><br>
+            <span v-if="displayMail"> <a v-bind:href="'mailto:' +  inputEmail" style="color:#3684c8;">{{ inputEmail }}</a></span>
+            <span v-else><a href="mailto:john.doe@team-scylla.fr" style="color:#3684c8;">john.doe@team-scylla.fr</a></span><br>
             <span v-if="displayPhone">{{ inputPhone }}</span>
             <span v-else>06 00 00 00 00</span>
           </p>
-          <p style="font-family: Helvetica, sans-serif; font-size: 12px; line-height: 14px; color: rgb(51, 51, 51); margin-top: 0px;" id="sigCompanyWrapper">
-            <span id="sigTitle"><a v-bind:href="inputLinkedIn" target="_blank">LinkedIn</a></span><br>
-            <span><a href="http://team-scylla.fr" id="sigWebsite" style="color:#428BCA;" rel="nofollow" target="_blank">https://team-scylla.fr</a></span> <br>
-            <span id="sigAddress" style="font-size: 10px; line-height: 14px; margin: 5px 0; display:block; opacity:0.8;"></span>
+          <p style="font-family: Helvetica, sans-serif; font-size: 12px; line-height: 14px; color: rgb(51, 51, 51); margin-top: 0px;">
+            <span style="font-size: 10px; line-height: 14px; margin: 5px 0; display:block; opacity:0.8;"><a v-bind:href="inputLinkedIn" target="_blank" style="color: black">LinkedIn</a></span>
+            <span><a href="http://team-scylla.fr" style="color:#3684c8;" rel="nofollow" target="_blank">https://team-scylla.fr</a></span> <br>
           </p>
         </div>
       </div>
@@ -79,7 +77,9 @@
     <div class="panel panel-default">
       <div class="panel-heading">Code html de votre signature</div>
       <div class="panel-body">
-        Panel content
+        <div class="panel-body">
+          <textarea id="htmlForm"></textarea>
+        </div>
       </div>
     </div>
   </div>
@@ -114,7 +114,23 @@
       },
       showPhone () {
         this.$data.displayPhone = true
+      },
+      updateHtml () {
+        this.$nextTick(function () {
+          window.$('#htmlForm').text(window.$('#signature').html())
+        })
       }
+    },
+    mounted () {
+      window.$('#htmlForm').text(window.$('#signature').html())
+    },
+    watch: {
+      inputFirstName: function () { this.updateHtml() },
+      inputLastName: function () { this.updateHtml() },
+      inputRole: function () { this.updateHtml() },
+      inputEmail: function () { this.updateHtml() },
+      inputPhone: function () { this.updateHtml() },
+      inputLinkedIn: function () { this.updateHtml() }
     }
   }
 </script>
@@ -122,5 +138,9 @@
 <style>
   .panel.first {
     margin-top: 50px;
+  }
+
+  textarea {
+    width: 100%;
   }
 </style>
