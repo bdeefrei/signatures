81ee29a0f2f7ab6398add1df34f9b03dad332298
Alexandre Martin <alexandre@m4rtin.xyz>
2018-05-30 21:30:46 +0200

    Update header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,14 +14,14 @@
 
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li><a href="http://team-scylla.fr/" target="_blank">BDE Scylla</a></li>
-          <li><a href="http://groupe-efrei.fr/" target="_blank">Groupe Efrei</a></li>
-          <li><a href="https://github.com/bde-groupe-efrei/signatures" target="_blank">GitHub du projet</a></li>
+          <li><a href="https://bde-efrei.fr/" target="_blank">BDE Overlord</a></li>
+          <li><a href="http://efrei.fr/" target="_blank">Efrei Paris</a></li>
+          <li><a href="https://github.com/bde-efrei-paris/signatures" target="_blank">GitHub du projet</a></li>
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="https://www.facebook.com/Liste-BDE-Scylla-1212790998839759/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-          <li><a href="https://github.com/bde-groupe-efrei" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.facebook.com/bdeoverlord/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+          <li><a href="https://github.com/bde-efrei-paris" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
         </ul>
       </div>
     </div>
